87def262954f85f547cac726a04a7c87fb61cb7d
mohsinnaqvipro <mohsinnaqvi.engr@gmail.com>
2021-01-16 15:22:07 +0500

    Add Routers

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express"),
   http = require("http");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const userRouter = require("./routes/userRouter");
 
 const hostName = "localhost";
-const port = 3000;
+const port = 4000;
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,12 +13,7 @@ app.use(bodyParser.json());
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
 
-app.use((req, res, next) => {
-  console.log(req.headers);
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end("<html><body><h1>This is an Express Server</h1></body></html>");
-});
+app.use("/users", userRouter);
 
 const server = http.createServer(app);
 
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,4 +4,46 @@ const { Router } = require("express");
 
 const userRouter = express.Router();
 
-user;
+userRouter.use(bodyParser.json());
+
+userRouter
+  .route("/")
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    next();
+  })
+  .get((req, res, next) => {
+    let data = [
+      {
+        name: "mohsin",
+        department: "software",
+      },
+      {
+        name: "sajjad",
+        department: "software",
+      },
+      {
+        name: "ahsan",
+        department: "software",
+      },
+    ];
+    console.log("Data: ", data);
+    res.end(JSON.stringify(data));
+  })
+  .post((req, res, next) => {
+    res.end(
+      "Will add the dish: " +
+        req.body.name +
+        " with details: " +
+        req.body.description
+    );
+  })
+  .put((req, res, next) => {
+    res.statusCode = 403;
+    res.end("PUT operation not supported on /dishes");
+  })
+  .delete((req, res, next) => {
+    res.end("Deleting all dishes");
+  });
+module.exports = userRouter;
